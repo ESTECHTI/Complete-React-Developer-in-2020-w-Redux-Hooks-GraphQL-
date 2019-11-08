282c135e87f87183b848cd1c4f89a8388275115c
Elisandro Silva de Oliveira <elisandro.oliveira@pampa.compasso>
2019-11-08 16:35:08 -0300

    First atualization

---
+++ monsters-rolodex/src/App.js
@@ -0,0 +1,39 @@
+import React,  { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+
+  constructor(){
+    super();
+
+    this.state = {
+      monsters: [
+        {
+          name: 'Frankenstein',
+          id: 'asc1'
+        },
+        {
+          name: 'Dracula',
+          id: 'asr2'
+        },
+        {
+          name: 'Zombie',
+          id: 'as1w1'
+        },
+      ]
+    }
+  }
+
+  render() {
+    return(
+      <div className='App'>
+        {
+          this.state.monsters.map(monster => (
+            <h1 key={monster.id}> { monster.name } </h1>
+            ))}
+      </div>
+    )
+  }
+}
+
+export default App;
