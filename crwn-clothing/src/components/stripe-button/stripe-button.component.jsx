9449ecff3f34690b381931d7b39354b71c1e7324
E.STECH <e.s.tech>
2019-12-25 18:44:24 -0300

    updating stripe integration a nd updating our favicon and title

---
+++ crwn-clothing/src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+    const priceForStripe = price * 100;
+    const publishablekey = 'pk_test_jL5KKubvjzLu4wXkmUM4IX5S001kzAj7Hk';
+
+const onToken = token => {
+    console.log(token);
+    alert('Payment Successful')
+}
+
+    return(
+        <StripeCheckout 
+            label='Pay Now'
+            name='CRWN Clothing Ltd.'
+            billingAddress
+            shippingAddress
+            image='https://svgshare.com/i/CUz.svg'
+            description={`Your total is $${price}`}
+            amount={priceForStripe}
+            panelLabel='Pay Now'
+            token={onToken}
+            stripeKey={publishablekey}
+        />
+    )
+} 
+
+export default StripeCheckoutButton;
