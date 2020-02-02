5a278e1bda323bf3bb92443c0b3d3c54680e9df3
E.STECH <e.s.tech>
2020-02-02 16:52:58 -0300

    Email SignIn Into Sagas

--- crwn-clothing/src/components/sign-in/sign-in.component.jsx
+++ crwn-clothing/src/components/sign-in/sign-in.component.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import FormInput from '../form-input/form-input.component';
 import CustomButtom from '../custom-button/custom-button.component';
 
-import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
+import { googleSignInStart, emailSignInStart } from '../../redux/user/user.actions';
+
 
 import './sign-in.styles.scss';
 
@@ -20,14 +22,11 @@ class SignIn extends React.Component {
     handleSubmit = async event => {
         event.preventDefault();
 
+       const { emailSignInStart } = this.props
        const { email, password } = this.state;
 
-       try {
-            await auth.signInWithEmailAndPassword(email, password);
-            this.setState({ email: '', password: '' });
-       } catch (error) {
-            console.log(error);
-       }
+       emailSignInStart(email, password)
+
     }
 
     handleChange = event => {
@@ -37,6 +36,7 @@ class SignIn extends React.Component {
     }
 
     render(){
+        const { googleSignInStart } = this.props
         return(
             <div className='sign-in'>
                 <h2>I already have an account</h2>
@@ -60,7 +60,10 @@ class SignIn extends React.Component {
                         required/>
                     <div className='buttons'>
                         <CustomButtom type='submit'>Sign in</CustomButtom>
-                        <CustomButtom onClick={signInWithGoogle} isGoogleSignIn>
+                        <CustomButtom
+                            type='button'
+                            onClick={googleSignInStart} 
+                            isGoogleSignIn>
                             Sign in with Google
                         </CustomButtom>
                     </div>
@@ -70,5 +73,10 @@ class SignIn extends React.Component {
     }
 }
 
-export default SignIn;
+const mapDispatchToProps = dispatch => ({
+    googleSignInStart: () => dispatch(googleSignInStart()),
+    emailSignInStart: (email, password) => dispatch(emailSignInStart({ email, password }))
+})
+
+export default connect(null, mapDispatchToProps)(SignIn);
 
---
+++ crwn-clothing/src/redux/user/user.sagas.js
@@ -0,0 +1,43 @@
+import { takeLatest, put, all, call } from 'redux-saga/effects';
+
+import UserActionsTypes from './user.types';
+
+import { auth, googleProvider, createUserProfileDocument } from '../../firebase/firebase.utils';
+import { googleSignInSuccess, googleSignInFailure, emailSignInSuccess, emailSignInFailure } from './user.actions';
+import UserActionTypes from './user.types';
+
+export function* signInWithGoogle() {
+    try {
+        const {user} = yield auth.signInWithPopup(googleProvider);
+        const userRef = yield call(createUserProfileDocument, user);
+        const userSnapShot = yield userRef.get();
+        yield put(googleSignInSuccess({ id: userSnapShot.id, ...userSnapShot.data() }))
+
+    } catch(error) {
+        yield put(googleSignInFailure(error));
+    }
+}
+
+export function* signInWithEmail({payload: {email, password}}) {
+    try {
+        const { user } = yield auth.signInWithEmailAndPassword(email, password);
+        const userRef = yield call(createUserProfileDocument, user);
+        const userSnapShot = yield userRef.get();
+        yield put(
+            emailSignInSuccess({ id: userSnapShot.id, ...userSnapShot.data() }))
+    } catch(error) {
+        yield put(emailSignInFailure(error))
+    }
+}
+
+export function* onGoogleSignInStart() {
+    yield takeLatest(UserActionsTypes.GOOGLE_SIGN_IN_START, signInWithEmail)
+}
+
+export function* onEmailSignInStart() {
+    yield takeLatest(UserActionTypes.EMAIL_SIGN_IN_START, )
+}
+
+export function* userSagas() {
+    yield all([call(onGoogleSignInStart), call(onEmailSignInStart)]);
+}
